c89b6c19fc44f1dbf0b0e289bc7560bc76023467
Bob <Bob@146.244.98.152>
2013-12-03 11:43:37 -0800

    Fixed FlowSplitter to extend WorkingFluidObject

--- src/powerplant/object/FlowSplitter.java
+++ src/powerplant/object/FlowSplitter.java
@@ -19,10 +19,48 @@
 
 package powerplant.object;
 
+import powerplant.fluid.Fluid;
+
 /**
  *
  * @author DoorKip
  */
-public class FlowSplitter {
+public class FlowSplitter extends WorkingFluidObject{
+
+	@Override
+	public void solve() {
+		//TODO Implement solve in powerplant.object.FlowSplitter
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public WorkingFluidObject setWorkingFluidInput(Fluid fluid) {
+		//TODO Implement setWorkingFluidInput in powerplant.object.FlowSplitter
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public WorkingFluidObject setWorkingFluidOutput(Fluid fluid) {
+		//TODO Implement setWorkingFluidOutput in powerplant.object.FlowSplitter
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Fluid getWorkingFluidInput() {
+		//TODO Implement getWorkingFluidInput in powerplant.object.FlowSplitter
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Fluid getWorkingFluidOutput() {
+		//TODO Implement getWorkingFluidOutput in powerplant.object.FlowSplitter
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public boolean isSolved() {
+		//TODO Implement isSolved in powerplant.object.FlowSplitter
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
 }
